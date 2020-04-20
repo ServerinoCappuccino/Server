1072857b8aee571b5f334ecea64bbbe1cf3ef3b7
Matteo <matteo.marcuzzo.it@gmail.com>
2020-04-20 17:02:37 +0200

    Just a commit message

--- script.sh
+++ script.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 JAR=server.jar
+echo '------------------'
+echo 'Starting server...'
+echo '------------------'
 
 while [ true ]; do
   java -Xmx1g -server -jar $JAR nogui
   if [ $? -eq 0 ]; then
     break
   fi
+  echo 'Debug'
 done
+echo '----------------------'
+echo 'Server closed. Saving!'
+echo '----------------------'
+sh ./save.sh
